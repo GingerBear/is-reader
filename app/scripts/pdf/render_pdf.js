40c5e4ea9085bf161b4af19646ef170dfa4ac31f
unknown <bbbear444@gmail.com>
2013-10-13 02:26:20 -0400

    restore working, hightlight working, reader css refined

--- app/scripts/pdf/render_pdf.js
+++ app/scripts/pdf/render_pdf.js
@@ -8,16 +8,16 @@
 // Author: Neil Ding
 
 var LoadPage = function () {
-    var args = Array.prototype.slice.call(arguments, 0);
-    console.log(args);
-    var file_name = args.shift();
-    var page = parseInt(args.shift());
-    var book_id = args.shift();
-    var pdfData = 'data/pdf/' + file_name; // should read from URL or DOM
-    var scale = 2; // Set this to whatever you want. This is basically the "zoom" factor for the PDF.
-    var highlights = []; // fill hights of current page by server
-    var pdf; // store pdf file into a locally global variable 
-
+    var args = Array.prototype.slice.call(arguments, 0)
+    , file_name = args.shift()
+    , page = parseInt(args.shift())
+    , book_id = args.shift()
+    , pdfData = 'data/pdf/' + file_name // should read from URL or DOM
+    , scale = 2 // Set this to whatever you want. This is basically the "zoom" factor for the PDF.
+    , highlights = [] // fill hights of current page by server
+    , pdf // store pdf file into a locally global variable 
+    , selectionPosition
+    , base_url = "http://localhost:3000/";
 
     // RENDER 
 
@@ -75,11 +75,11 @@ var LoadPage = function () {
         var $textLayerDiv = jQuery("<div />")
             .addClass("textLayer")
             .css("height", viewport.height + "px")
-            .css("width", viewport.width + "px")
-            .offset({
-                top: canvasOffset.top,
-                left: canvasOffset.left
-            });
+            .css("width", viewport.width + "px");
+            // .offset({
+            //     top: canvasOffset.top,
+            //     left: canvasOffset.left
+            // });
 
         $pdfContainer.append($textLayerDiv);
 
@@ -108,17 +108,11 @@ var LoadPage = function () {
         });
     }
 
-    // EVENTS
-    
-    window.onload = function () {
-        loadPdf(pdfData);
-    };
-
     function saveProgress(page) {
         $.ajax({
             type: 'PUT',
-            url: "http://is-reader.herokuapp.com/book/" + book_id,
-            //url: "http://localhost:3000/book/" + book_id,
+            //url: "http://is-reader.herokuapp.com/book/" + book_id,
+            url: base_url + 'book/' + book_id,
             data: {last_page: page},
             dataType: 'json',
             success: function(data){
@@ -127,30 +121,14 @@ var LoadPage = function () {
         });
     }
 
-    $('.next-page').click(function () {
-        page = page + 1;
-        highlights = [];
-        pageGoto(page);
-
-        // code : track progross to server
-
-    });
-
-    $('.pre-page').click(function () {
-        page = page - 1;
-        highlights = [];
-        pageGoto(page);
-
-        // code : track progross to server
-
-    });
-
     function pageGoto(p) {
         page = p;
         saveProgress(page);
-        jQuery("#pdfContainer").html("");
+        $("#pdfContainer").html("");
         $("html, body").animate({ scrollTop: 0 }, 0);
-        pdf.then(renderPdf);        
+        loadNotes(page, function() {
+            pdf.then(renderPdf);                
+        });    
     }
 
     function hideToolPop() {
@@ -188,13 +166,39 @@ var LoadPage = function () {
         return selection;
     }
 
+    var parseHL = function(notes) {
+        for (var i = notes.length - 1; i >= 0; i--) {
+            var pos = notes[i].position.split(',');;
+            highlights.push({                
+                startContainer : pos[0], 
+                endContainer : pos[1], 
+                startOffset : pos[2], 
+                endOffest : pos[3]
+            });
+        };
+    };
+
+    var loadNotes = function(page, callback) {
+        $.ajax({
+            type: 'GET',
+            //url: "http://is-reader.herokuapp.com/book/" + book_id,
+            url: base_url + 'book/' + book_id + "/" + page,
+            dataType: 'jsonp',
+            success: function(data){
+                parseHL(data);
+                if (callback) callback();
+            }
+        });
+        return this;
+    };
+
     var hlByOffset = (function () {
         var commonContainer = ".textLayer";
         var getEleByIndex = function (container) {
             return $(commonContainer + ' div:eq(' + container + ')')
         }
         var wrapByOffset = function ($el, from, to, wrapperTag) {
-            var len = $el.html().length;
+            var len = $el.html().length || 0;
             from = from || 0;
             to = to || len;
             // console.log(from); 
@@ -202,24 +206,57 @@ var LoadPage = function () {
             $el.html($el.html().substring(0, from) + "<span class='hl-text'>" + $el.html().substring(from, to) + "</span>" + $el.html().substring(to));
         }
         return function (startContainer, endContainer, startOffset, endOffest) {
+            console.log(startContainer + ' |' + endContainer + ' |' + startOffset + ' |' + endOffest);
             wrapByOffset(getEleByIndex(startContainer), startOffset, undefined);
             wrapByOffset(getEleByIndex(endContainer), undefined, endOffest);
-            for (var i = startContainer + 1; i < endContainer; i++) {
+            for (var i = parseInt(startContainer) + 1; i < parseInt(endContainer); i++) {
+                console.log(i);
                 wrapByOffset(getEleByIndex(i), undefined, undefined);
             };
         }
     })();
 
+    var saveNote = function (ifHighlight, notes, callback) {
+        $.ajax({
+            type: 'POST',
+            url: base_url + "note",
+            data: {
+                book_id: book_id,
+                user_id: 'Neil',
+                page: page,
+                position: selectionPosition,
+                is_hightlight: ifHighlight,
+                notes: notes || ''
+            },
+            dataType: 'json',
+            success: function(data){
+                var pos = selectionPosition.split(',');
+                hlByOffset(pos[0], pos[1], pos[2], pos[3])
+                if (callback) callback();
+            }
+        });
+    };
+
+    $('.next-page').click(function () {
+        page = page + 1;
+        highlights = [];
+        pageGoto(page);
+        // code : track progross to server
+
+    });
+
+    $('.pre-page').click(function () {
+        page = page - 1;
+        highlights = [];
+        pageGoto(page);
+
+        // code : track progross to server
+
+    });
 
     $('#pdfContainer').on('mouseup', function(e) {
         var selection = getSelectionObj();
         if (selection != "") {
-
-
-
-            // load highlights
-            // hlByOffset(9, 18, 14, 22)
-
 
 
             console.log("=========== Text Selection Captured ===========");
@@ -228,6 +265,11 @@ var LoadPage = function () {
             console.log("Ending div index: " + $(selection.getRangeAt(0).endContainer.parentElement).index());
             console.log("Starting div cursor index: " + selection.getRangeAt(0).startOffset);
             console.log("Ending div cursor index: " + selection.getRangeAt(0).endOffset);
+
+            selectionPosition = $(selection.getRangeAt(0).startContainer.parentElement).index() + 
+            ',' + $(selection.getRangeAt(0).endContainer.parentElement).index() + 
+            ',' + selection.getRangeAt(0).startOffset + 
+            ',' + selection.getRangeAt(0).endOffset;
 
             // var endEl = $(selection.getRangeAt(0).endContainer.parentElement);
 
@@ -253,11 +295,20 @@ var LoadPage = function () {
         e.stopPropagation();    
     });
 
-    $('.tool-pop .hl-btn').click(function() {
+    $('.tool-pop ul').click(function(e) {
+        e.preventDefault();
         hideToolPop();
+        var target = $(e.target);
+        if (target.is('.hl-btn')) {
+            saveNote(true, '', function (argument) {
+                console.log('HL Saved!');
+            });
+        } else if (target.is('.note-btn')) {
+            alert('note');
+        } else if (target.is('.share-btn')) {
+            alert('share');
+        }
     });
-
-    // just for demo
 
     return {
 
@@ -271,13 +322,24 @@ var LoadPage = function () {
             };
 
             pageGoto(p);
-
         },
 
         pageGoto: function(p) {
             pageGoto(p);
-        }
+        },
 
+        refresh: function() {
+            $("#pdfContainer").html("");
+            $("html, body").animate({ scrollTop: 0 }, 0);
+            pdf.then(renderPdf);     
+            return this;
+        },
+
+        init: function() {            
+            loadNotes(page, function() {
+                loadPdf(pdfData);            
+            });    
+        }
     }
 
 };
